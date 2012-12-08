6478c71d5d68f1c569c7d2245cf33ed84ea0422a
Vitaliy Kustov <kustov.vitalik@gmail.com>
2012-12-08 20:47:07 +0700

    Добавил SQL базы, профайлер, модуль событий12

--- modules/profilertoolbar/init.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-// include vendor FireBug
-if(ProfilerToolbar::cfg('firebug.enabled') && !class_exists('FireBug')){
-  require_once Kohana::find_file('vendor/FirePHPCore','FirePHP.class','php');
-}
-
